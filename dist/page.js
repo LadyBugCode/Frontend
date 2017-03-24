c802a1ae7989b988ba9c9ebaf1ddb6a5a906497e
Yulia Suhomlinova <julssuhomli@gmail.com>
2017-03-24 21:01:50 +0300

    first commit

---
+++ dist/page.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+
+  $('.big-nav--item').hover(
+         function(){ $(this).addClass('big-nav--hovered') },
+         function(){ $(this).removeClass('big-nav--hovered') }
+    )
+
+});
